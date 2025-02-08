c1e143ff0c2ffa80ea4880eab7ff0a95f2339a5b
luo9tian <2238553201@qq.com>
2025-02-08 12:00:24 +0800

    基于单个文件的动静生成

---
+++ luoxing-generator-basic/src/main/java/com/luojiu/model/MainTemplateConfig.java
@@ -0,0 +1,10 @@
+package com.luojiu.model;
+
+import lombok.Data;
+
+@Data
+public class MainTemplateConfig {
+    private String author;
+    private String outputText;
+    private boolean loop;
+}
